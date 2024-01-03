4829e11d2fcbf8127228038710ef915bd3e1d517
SmallTeddy <zhangzeming1212@gmail.com>
2024-01-03 23:26:18 +0800

    feat: add 03 nav

--- .vitepress/config/sidebar.ts
+++ .vitepress/config/sidebar.ts
@@ -6,7 +6,8 @@ export const sidebar = {
       link: '/docs/01',
       items: [
         { text: '第一章 技术栈简介', link: '/docs/01' },
-        { text: '第二章 环境部署', link: '/docs/02' }
+        { text: '第二章 环境部署', link: '/docs/02' },
+        { text: '第三章 项目创建', link: '/docs/03' }
       ]
     }
   ]
