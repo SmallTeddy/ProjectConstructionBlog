e43f531966d0abf7874663783c5858ef9c10b66e
SmallTeddy <zhangzeming1212@gmail.com>
2024-01-04 14:39:24 +0800

    fix: home menu

--- .vitepress/theme/components/vp-home.vue
+++ .vitepress/theme/components/vp-home.vue
@@ -18,16 +18,18 @@
     <f-divider margin="40px" color="#eef" />
 
     <f-text bold block>指北目录</f-text>
-    <f-text bold block>第一章 技术栈简介(开篇)</f-text>
-    <f-text block>第二章 环境部署(Node等环境安装)</f-text>
-    <f-text block>第三章 项目创建(Vite项目初始化)</f-text>
-    <f-text bold block>第四章 认识项目目录(项目整体介绍)</f-text>
-    <f-text>第五章 Element-Plus组件库安装和使用(Element-Plus基础配置)</f-text>
-    <f-text>第六章 样式格式化(Sass配置)</f-text>
-    <f-text>第七章 路由配置(vue-router深入解读)</f-text>
-    <f-text>第八章 Layout组件(Element-Plus的使用)</f-text>
-    <f-text>第九章 登录开发(Pinia的使用和数据持久化)</f-text>
-    <f-text>第十章 请求配置(Axios请求和响应拦截)</f-text>
+    <f-link href="/ProjectConstructionBlog/docs/01.html">第一章 技术栈简介(开篇)</f-link>
+    <f-link href="/ProjectConstructionBlog/docs/02.html">第二章 环境部署(Node等环境安装)</f-link>
+    <f-link href="/ProjectConstructionBlog/docs/03.html">第三章 项目创建(Vite项目初始化)</f-link>
+    <f-link href="/ProjectConstructionBlog/docs/04.html">第四章 认识项目目录(项目整体介绍)</f-link>
+    <f-link href="/ProjectConstructionBlog/docs/05.html">第五章 Element-Plus组件库安装和使用(Element-Plus基础配置)</f-link>
+    <f-link href="/ProjectConstructionBlog/docs/06.html">第六章 样式格式化(Sass配置)</f-link>
+    <f-link href="/ProjectConstructionBlog/docs/07.html">第七章 路由配置(vue-router深入解读)</f-link>
+    <f-link href="/ProjectConstructionBlog/docs/08.html">第八章 Layout组件(Element-Plus的使用)</f-link>
+    <f-link href="/ProjectConstructionBlog/docs/09.html">第九章 登录开发(Pinia的使用和数据持久化)</f-link>
+    <f-link href="/ProjectConstructionBlog/docs/10.html">第十章 请求配置(Axios请求和响应拦截)</f-link>
+    <f-link href="/ProjectConstructionBlog/docs/11.html">第十一章 基础界面开发 (组件封装和使用)</f-link>
+    <f-link href="/ProjectConstructionBlog/docs/12.html">第十二章 常用工具函数 (Utils配置)</f-link>
   </f-main>
 </template>
 
@@ -44,7 +46,8 @@
   margin-left: 10px;
 }
 
-.f-text {
+.f-text,
+.f-link {
   display: block;
   line-height: 40px;
 }
