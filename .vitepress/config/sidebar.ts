54386114758bbf3366219893a907f3683f825866
SmallTeddy <zhangzeming1212@gmail.com>
2024-01-04 10:31:00 +0800

    feat: add menu route and file

--- .vitepress/config/sidebar.ts
+++ .vitepress/config/sidebar.ts
@@ -9,6 +9,12 @@ export const sidebar = {
         { text: '第二章 环境部署', link: '/docs/02' },
         { text: '第三章 项目创建', link: '/docs/03' },
         { text: '第四章 认识项目目录', link: '/docs/04' },
+        { text: '第五章 组件库安装和使用', link: '/docs/05' },
+        { text: '第六章 样式格式化', link: '/docs/06' },
+        { text: '第七章 路由配置', link: '/docs/07' },
+        { text: '第八章 Layout组件', link: '/docs/08' },
+        { text: '第九章 登录开发', link: '/docs/09' },
+        { text: '第十章 请求配置', link: '/docs/10' },
       ]
     }
   ]
