807b38d7b6812f25f10dd972f8c8fc7935ff7f7c
SmallTeddy <zhangzeming1212@gmail.com>
2024-01-04 11:03:22 +0800

    fix: change route

--- .vitepress/config/sidebar.ts
+++ .vitepress/config/sidebar.ts
@@ -1,21 +1,14 @@
 export const sidebar = {
   '/docs/': [
-    {
-      text: ' 查看文档',
-      collapsible: true,
-      link: '/docs/01',
-      items: [
-        { text: '第一章 技术栈简介', link: '/docs/01' },
-        { text: '第二章 环境部署', link: '/docs/02' },
-        { text: '第三章 项目创建', link: '/docs/03' },
-        { text: '第四章 认识项目目录', link: '/docs/04' },
-        { text: '第五章 组件库安装和使用', link: '/docs/05' },
-        { text: '第六章 样式格式化', link: '/docs/06' },
-        { text: '第七章 路由配置', link: '/docs/07' },
-        { text: '第八章 Layout组件', link: '/docs/08' },
-        { text: '第九章 登录开发', link: '/docs/09' },
-        { text: '第十章 请求配置', link: '/docs/10' },
-      ]
-    }
+    { text: '第一章 技术栈简介', link: '/docs/01' },
+    { text: '第二章 环境部署', link: '/docs/02' },
+    { text: '第三章 项目创建', link: '/docs/03' },
+    { text: '第四章 认识项目目录', link: '/docs/04' },
+    { text: '第五章 组件库安装和使用', link: '/docs/05' },
+    { text: '第六章 样式格式化', link: '/docs/06' },
+    { text: '第七章 路由配置', link: '/docs/07' },
+    { text: '第八章 Layout组件', link: '/docs/08' },
+    { text: '第九章 登录开发', link: '/docs/09' },
+    { text: '第十章 请求配置', link: '/docs/10' },
   ]
 }
